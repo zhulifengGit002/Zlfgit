f416ccb5a966bdff35b59b19aedb874300ac73bc
zhulifeng <169687107@qq.com>
2016-12-20 10:21:16 +0800

    342q

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $signPackage = $jssdk->GetSignPackage();
 			<h3><a href="aixianfeng/shouye.html">爱鲜蜂</a></h3>
 			<h2>这是我在master新添加的内容</h2>
             <button onclick="getLocation()">dianji</button>
+            <button onclick="getPosition()">查看</button>
 		</div>		
 	</body>
 	<script type="text/javascript">
@@ -61,17 +62,29 @@ $signPackage = $jssdk->GetSignPackage();
         'openCard'
       ]
   });
-         function getLocation(){
-                 wx.getLocation({
-                type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                success: function (res) {
-                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                    var speed = res.speed; // 速度，以米/每秒计
-                    var accuracy = res.accuracy; // 位置精度
-                    console.log(latitude);
-                }
-            });  
-         }
+    var latitude =0,longitude=0;
+function getLocation(){
+     wx.getLocation({
+    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    success: function (res) {
+        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        var speed = res.speed; // 速度，以米/每秒计
+        var accuracy = res.accuracy; // 位置精度
+    }
+});  
+}
+function getPosition(){
+    wx.openLocation({
+        latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+        longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+        name: '', // 位置名
+        address: '', // 地址详情说明
+        scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+        infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+    });
+    console.log(latitude);
+    console.log(longitude);
+    }
 	</script>
 </html>
